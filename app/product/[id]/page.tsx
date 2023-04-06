c08763c08b12d6d4e0aa83f80c47a23c51187b7b
Thi-Tracey_Nguyen <traceynguyen.1988@gmail.com>
2023-04-06 13:41:12 +1000

    implemented cart using zustand

--- app/product/[id]/page.tsx
+++ app/product/[id]/page.tsx
@@ -14,7 +14,7 @@ export default async function Product({searchParams}: SearchParamTypes) {
       <div className='font-medium text-gray-700'>
         <h1 className='text-2xl py-2'>{searchParams.name}</h1>
         <p className='py-2'>{searchParams.description}</p>
-        <p>{searchParams.features}</p>
+        <p className='py-2'>{searchParams.features}</p>
         <div className='flex gap-2'>
           <p className='font-bold text-teal-700'>
             {searchParams.unit_amount && formatPrice(searchParams.unit_amount)}
