bfab2a760b3570fecb36b86f803a0b5f4a68d8da
Thi-Tracey_Nguyen <traceynguyen.1988@gmail.com>
2023-04-11 14:27:19 +1000

    small changes to dashboard styling

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -2,6 +2,10 @@ import { PrismaClient } from '@prisma/client'
 import { getServerSession } from 'next-auth'
 import { authOptions } from '@/pages/api/auth/[...nextauth]'
 import formatPrice from '@/util/PriceFormat'
+import Image from 'next/image'
+
+//refetch the data to reflect latest order's status - NextJS
+export const revalidate = 0
 
 const fetchOrders = async () => {
   const prisma = new PrismaClient()
@@ -13,7 +17,10 @@ const fetchOrders = async () => {
 
   // find all orders of the user
   const orders = await prisma.order.findMany({
-    where: { userId: user?.user?.id }, 
+    where: { 
+      userId: user?.user?.id,
+      status: 'complete' 
+    }, 
     include: { products: true },
   })
   return orders
@@ -36,19 +43,34 @@ export default async function DashBoard() {
   return (
     <div className='font-medium'>
       {orders.map((order) => (
-        <div key={order.id} className='rounded-lg'>
-          <h2>Order reference: {order.id}</h2>
-          <p>Time: {new Date(order.createdDate)}</p>
-          <p className='text-md py-2'>
+        <div key={order.id} className='rounded-lg p-8 my-12'>
+          <h2 className='text-xs font-medium'>Order reference: {order.id}</h2>
+          <p className='text-xs'>Time: {new Date(order.createdDate).toString()}</p>
+          <p className='text-md py-2 text-xs'>
             Status: {' '}
             <span 
-              className={`${
-                order.status === 'complete' ? 'bg-teal-500' : 'bg-orange-500'
-            } text-white py-1 rounded-md px-2 mx-2 text-sm`}>
+              className={'bg-teal-500 text-white py-1 rounded-md px-2 mx-2 text-sm'}>
               {order.status}
             </span>
           </p>
           <p className='font-medium'>Total: {formatPrice(order.amount)}</p>
+          <div className='text-sm lg:flex items-center lg:items-baseline gap-4'>
+            {order.products.map((product) => (
+              <div className='py-2' key={product.id}>
+                <h2 className='py-2'>{product.name}</h2>
+                <div className='flex items-baseline gap-4'>
+                  <Image
+                    src={product.image!}
+                    width={36}
+                    height={36}
+                    alt={product.name}
+                  />
+                  <p>{formatPrice(product.unit_amount)}</p>
+                  <p>Quantity: {product.quantity}</p>
+                </div>
+              </div>
+            ))}
+          </div>
         </div>
       ))}
     </div>
